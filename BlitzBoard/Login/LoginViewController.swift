30ce70f35a44b29cce6c89a306a97fa9f947268b
kiasha10 <kiasharangasamy2000@gmail.com>
2024-04-14 15:57:42 +0200

    Designed UI for Login Storyboard and added View Controller

--- BlitzBoard/Login/LoginViewController.swift
+++ BlitzBoard/Login/LoginViewController.swift
@@ -5,4 +5,37 @@
 //  Created by Kiasha Rangasamy on 2024/04/11.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Login Outlet
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    // Action
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            
+            // Empty field alert message
+            showAlert(message: "Please enter both email and password.")
+            return
+        }
+        
+    }
+    
+    // MARK: - Helper Methods
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+}
