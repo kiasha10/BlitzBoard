a489c6e64c937293d5aba6e0c1928642b08746df
kiasha10 <kiasharangasamy2000@gmail.com>
2024-05-07 11:41:49 +0200

    PR -  API fix

--- BlitzBoard/Features/Repository/HomeScreenRepository.swift
+++ BlitzBoard/Features/Repository/HomeScreenRepository.swift
@@ -20,10 +20,6 @@ class LeagueTableRepository: LeagueTableRepositoryType {
     private let apiHandler = APIHandler()
     
     func fetchLeagueTableResults(completion: @escaping (LeagueTableResult)) {
-        apiHandler.request(endpoint: "https://apiv3.apifootball.com/" +
-                           "?action=get_standings&league_id=152&" 
-                           + "APIkey=afd5a1ede5598029ca62b14f758edd87481d42c83fecb45afb8fa613c50efb7c",
-                           method: "GET",
-                           completion: completion)
+        apiHandler.request(endpoint: EndPoint.leagueTableEndpoint, method: "GET", completion: completion)
     }
 }
