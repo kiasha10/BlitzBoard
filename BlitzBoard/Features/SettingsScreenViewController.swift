78da7512beacc4b79fa094a8eb9f9753cedc223d
kiasha10 <kiasharangasamy2000@gmail.com>
2024-05-19 11:47:57 +0200

    PR - Added a warning to Settings file

--- BlitzBoard/Features/SettingsScreenViewController.swift
+++ BlitzBoard/Features/SettingsScreenViewController.swift
@@ -3,8 +3,8 @@
 //  BlitzBoard
 //
 //  Created by Kiasha Rangasamy on 2024/05/06.
-//
 
+// #warning Settings File will be used in the future
 import UIKit
 
 class SettingsScreenViewController: UIViewController {
