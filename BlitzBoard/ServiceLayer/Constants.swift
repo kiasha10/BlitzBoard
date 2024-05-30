8ea68ae667b7f2a48d3d2cdb6f158f9c68cc9cc0
kiasha10 <kiasharangasamy2000@gmail.com>
2024-05-30 07:35:21 +0200

    UI clean up on storyboards and xib cells

--- BlitzBoard/ServiceLayer/Constants.swift
+++ BlitzBoard/ServiceLayer/Constants.swift
@@ -19,7 +19,7 @@ struct EndPoint {
     "APIkey=6f272244c8ec25329b4c524bf4a60f6ee13a7881fade63ec99acda4ca0437d5f"
     
     static let gameFixtures =
-    "https://apiv3.apifootball.com/?action=get_events&from=2024-05-19&to=2024-05-20&league_id=152&" +
+    "https://apiv3.apifootball.com/?action=get_events&from=2024-05-04&to=2024-05-19&league_id=152&" +
     "APIkey=74b020fc0a9030ec2a6c0306d1db570ceacebbef52143f3185e22732224c0bc9"
 }
 
